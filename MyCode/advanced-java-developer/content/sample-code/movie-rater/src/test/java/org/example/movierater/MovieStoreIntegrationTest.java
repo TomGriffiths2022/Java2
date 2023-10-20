a8afeea36e4e5d9cb5edfa1da0ba048e90ccf57c
Tomos Griffiths <tomos.griffiths@dvla.gov.uk>
2023-10-20 13:41:26 +0100

    pre-merge after updates

--- MyCode/advanced-java-developer/content/sample-code/movie-rater/src/test/java/org/example/movierater/MovieStoreIntegrationTest.java
+++ MyCode/advanced-java-developer/content/sample-code/movie-rater/src/test/java/org/example/movierater/MovieStoreIntegrationTest.java
@@ -11,5 +11,68 @@ public class MovieStoreIntegrationTest {
         store = MovieStore.getInstance(new CsvFileMovieRepo("./movies.csv"));
     }
 
-    // TODO code tests for each MovieStore method
+    @ParameterizedTest(name = "Threads = {0}")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void testFetchMovieManyThreads(int threadCount) throws IOException {
+        var service = Executors.newFixedThreadPool(threadCount);
+        var futures = new LinkedList<Future<String>>();
+        getRandomImdbIDs(5).forEach(imdbID -> {
+            futures.add(service.submit(() -> {
+                try {
+                    return fetchMovie(omdbApiKey, imdbID);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    return null;
+                }
+            }));
+        });
+        futures.forEach(future -> {
+            try {
+                var movie = future.get();
+                System.out.println(movie);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        });
+    }
+
+    @ParameterizedTest(name = "Threads = {0}")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void testFetchMovieManyThreadsFiftyMovies(int threadCount) throws IOException {
+        var service = Executors.newFixedThreadPool(threadCount);
+        var futures = new LinkedList<Future<String>>();
+        getRandomImdbIDs(50).forEach(imdbID -> {
+            futures.add(service.submit(() -> {
+                try {
+                    return fetchMovie(omdbApiKey, imdbID);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    return null;
+                }
+            }));
+        });
+        futures.forEach(future -> {
+            try {
+                var movie = future.get();
+                System.out.println(movie);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        });
+    }
+
+
+    @Test
+    void testFetchMovieManyTheadsWithParallelStream() throws IOException {
+        getRandomImdbIDs(50).parallelStream()
+                .map(imdbID -> {
+                    try {
+                        return fetchMovie(omdbApiKey, imdbID);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        return null;
+                    }
+                })
+                .forEach(System.out::println);
+    }
 }
